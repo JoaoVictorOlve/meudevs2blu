6f2314c983fbc557e403adbcacfe2c54d5483b6d
Joao Victor De Oliveira <joao.oliveira@proway.treina>
2022-12-08 21:55:34 -0300

    flask

--- 2022_12_07_flask/main.py
+++ 2022_12_07_flask/main.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from pessoa import Pessoa
 
+pessoa1 = Pessoa('Haiko', '17', '1.50')
+pessoa2 = Pessoa('Jean', '42', '1.84')
+pessoa3 = Pessoa('Leandro', '27', '1.94')
+
+lista = [pessoa1, pessoa2, pessoa3]
+
+#
 app = Flask(__name__)
 
 
 
+#
 @app.route('/')
-def inicio():
-    pessoa1 = Pessoa('Haiko', '17', '1.50')
-    pessoa2 = Pessoa('Jean', '42', '1:84')
-    pessoa3 = Pessoa('Gisele', '16', '1:30')
-    pessoa4= Pessoa('Leandro', '27', '1:94')
+def index():
+    return render_template('index.html', titulo='Lista' ,pessoas = lista)
 
-    lista = [pessoa1, pessoa2, pessoa3, pessoa4]
-    return render_template('index.html', titulo='Gerola', pessoas = lista)
-
-@app.route('/new')
+@app.route('/novo')
 def novo():
-    return 'New Route'
+    return render_template('novo.html', titulo = 'Cadastro Pessoa')
 
-app.run()
+
+@app.route('/criar', methods = ['POST'])
+def criar():
+    nome = request.form['nome']
+    idade = request.form['idade']
+    altura = request.form['altura']
+
+    cad_pessoa = Pessoa(nome, idade, altura)
+
+    lista.append(cad_pessoa)
+
+    return redirect('/')
+
+app.run(debug=True)
+
